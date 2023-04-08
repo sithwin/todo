b5945e703c93f7687b4500cb3500e84685d02827
Si Thu Win <sthuwin@gmail.com>
2023-04-08 21:33:31 +1000

    added CSS

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { TodoModel } from "../models/todoModel";
 import TodoItem from "./TodoItem";
+import classes from "./Todo.module.css";
 
 const Todo: React.FC<{ todos: TodoModel[] }> = (props) => {
   return (
     <div>
       <h1>Todo List</h1>
-      <ul>
+      <ul className={classes.todos}>
         {props.todos.map((todo) => (
           <TodoItem key={todo.id} text={todo.title} />
         ))}
